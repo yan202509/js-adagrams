09d4cb6270be7170fb93ad20b5003c62b00aab4c
YanYi Chen <vickyeah13@gmail.com>
2025-11-23 00:46:26 -0800

    fix wave 1, now use index to pop

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,3 +1,5 @@
+
+
 export const LETTER_POOL = {
   A: 9,
   B: 2,
@@ -43,13 +45,34 @@ export const drawLetters = () => {
       lettersList.push(letter);
   }
 
+
+//   const lettersInHand = [];
+//   for (let i = 0; i < NUMBER_IN_ONEHAND; i++) {
+//     const randomSelect = Math.floor(Math.random() * lettersList.length);
+//     const tenCount = lettersList.splice(randomSelect, 1)[0];
+//     lettersInHand.push(tenCount.toLocaleUpperCase());
+//   }
+//   return lettersInHand;
+// };
+
   // Draw 10 random letters from an object
   // Pool size is the same for each drawing
+  // big O, by place the letter to the end,
+  // letter do not need to move up one index from
+  // where the random letter is removed
   const lettersInHand = [];
   for (let i = 0; i < NUMBER_IN_ONEHAND; i++) {
     const randomSelect = Math.floor(Math.random() * lettersList.length);
-    const tenCount = lettersList.splice(randomSelect, 1)[0];
-    lettersInHand.push(tenCount.toLocaleUpperCase());
+
+    const lastIndexLetter = lettersList.length - 1;
+
+    const randomLetterSelected = lettersList[randomSelect];
+    lettersList[randomSelect] = lettersList[lastIndexLetter];
+    lettersList[lastIndexLetter] = randomLetterSelected;
+
+    // remove the last element of the array by .pop
+    const tenCount = lettersList.pop();
+    lettersInHand.push(tenCount.toUpperCase());
   }
   return lettersInHand;
 };
